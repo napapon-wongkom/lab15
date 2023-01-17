c8b393f4a28968d958ecb9b29ff3d1168418527a
napapon-wongkom <dot.wongkom@gmail.com>
2023-01-17 17:44:05 +0700

    lab15_2.cpp

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,40 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void inputMatrix(double A[][N]){
+	for(int x=0;x<N;x++){
+		cout<<"Row "<<x+1<<": ";
+		for(int y=0;y<N;y++){
+			cin>>A[x][y];
+		}
+	}
+}
+
+void findLocalMax(const double A[][N],bool B[][N]){
+	for(int x=0;x<N;x++){
+		for(int y=0;y<N;y++){
+			if(x==0||y==0||x==N-1||y==N-1){
+				B[x][y]=0;
+			}else{
+				if(A[x][y]>=A[x-1][y] && A[x][y]>=A[x+1][y] && A[x][y]>=A[x][y-1] && A[x][y]>=A[x][y+1]){
+				B[x][y]=1;
+			    }else B[x][y]=0;
+			}
+			
+		}
+	}
+}
+
+void showMatrix(const bool A[][N]){
+	for(int x=0;x<N;x++){
+		for(int y=0;y<N;y++){
+			if(A[x][y]==1){
+				cout<<"1 ";
+			}else if(A[x][y]==0){
+				cout<<"0 ";
+			}
+
+		}
+		cout<<"\n";
+	}
+}
